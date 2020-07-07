81f20ed8a56da17116d10542d96cb74edfdc54a8
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-07 15:25:04 +1200

    replace the initial hero property to selectedHero with type Hero and add the click event handler by writting the onSelect method, which will receive the selected hero as parameter from HTML template and will asign it to the selectedHero property

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -10,6 +10,12 @@ import { HEROES } from '../../mock-heroes';
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
 
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
