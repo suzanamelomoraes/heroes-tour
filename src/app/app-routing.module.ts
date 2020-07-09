4175c520b7535bdf7960ef0b920e2164323db794
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-09 16:52:55 +1200

    import dashboard component and add a path to int in Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
