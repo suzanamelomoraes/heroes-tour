f1dafb267f65d4b0a6690ac75253efa5966987b9
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-15 19:33:25 +1200

    add a remainder comment

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -15,6 +15,7 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
     this.messagesService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find((hero) => hero.id === id));
   }
